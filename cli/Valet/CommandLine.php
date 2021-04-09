e17b0aa96692030465f6a59e7a46f73ec10b3eac
Sergio G. Cruz Espinoza <seguce92@gmail.com>
2021-04-09 17:31:20 -0400

    @update commandline

--- cli/Valet/CommandLine.php
+++ cli/Valet/CommandLine.php
@@ -10,31 +10,28 @@ class CommandLine
      * Simple global function to run commands.
      *
      * @param string $command
-     *
      * @return void
      */
     public function quietly($command)
     {
-        $this->runCommand($command.' > /dev/null 2>&1');
+        $this->runCommand($command . ' > /dev/null 2>&1');
     }
 
     /**
      * Simple global function to run commands.
      *
      * @param string $command
-     *
      * @return void
      */
     public function quietlyAsUser($command)
     {
-        $this->quietly('sudo -u '.user().' '.$command.' > /dev/null 2>&1');
+        $this->quietly('sudo -u ' . user() . ' ' . $command . ' > /dev/null 2>&1');
     }
 
     /**
      * Pass the command to the command line and display the output.
      *
      * @param string $command
-     *
      * @return void
      */
     public function passthru($command)
@@ -47,7 +44,6 @@ class CommandLine
      *
      * @param string   $command
      * @param callable $onError
-     *
      * @return string
      */
     public function run($command, callable $onError = null)
@@ -60,12 +56,11 @@ class CommandLine
      *
      * @param string   $command
      * @param callable $onError
-     *
      * @return string
      */
     public function runAsUser($command, callable $onError = null)
     {
-        return $this->runCommand('sudo -u '.user().' '.$command, $onError);
+        return $this->runCommand('sudo -u ' . user() . ' ' . $command, $onError);
     }
 
     /**
@@ -73,14 +68,22 @@ class CommandLine
      *
      * @param string   $command
      * @param callable $onError
-     *
      * @return string
      */
     protected function runCommand($command, callable $onError = null)
     {
-        $onError = $onError ?: function () {};
+        $onError = $onError ?: function () {
+        };
 
-        $process = new Process($command);
+        // Symfony's 4.x Process component has deprecated passing a command string
+        // to the constructor, but older versions (which Valet's Composer
+        // constraints allow) don't have the fromShellCommandLine method.
+        // For more information, see: https://github.com/laravel/valet/pull/761
+        if (method_exists(Process::class, 'fromShellCommandline')) {
+            $process = Process::fromShellCommandline($command);
+        } else {
+            $process = new Process($command);
+        }
 
         $processOutput = '';
         $process->setTimeout(null)->run(function ($type, $line) use (&$processOutput) {
